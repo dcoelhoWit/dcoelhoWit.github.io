c449587f9de62fdbea7583440759cdd48595409a
Diogo Coelho <116639692+dcoelhoWit@users.noreply.github.com>
2024-05-26 14:51:32 +0100

    Add files via upload

--- expressoes/js/script.js
+++ expressoes/js/script.js
@@ -7,27 +7,15 @@ function send(level) {
 
 	if (level == "GTEFST") {
 		correctAnswer = "nem que a vaca tussa";
-		correctAnswer2 = "nem que a vaca tussa";
-		correctAnswer3 = "nem que a vaca tussa";
-
 		destination = "GTESND.html";
 	} else if (level == "GTESND") {
-		correctAnswer = "cair o carmo e a trindade";
-		correctAnswer2 = "cair o carmo e a trindade";
-		correctAnswer3 = "cair o carmo e a trindade";
-
+		correctAnswer = "carmo e a trindade";
 		destination = "GTETRD.html";
 	} else if (level == "GTETRD") {
 		correctAnswer = "tempo da maria cachucha";
-		correctAnswer2 = "do tempo da maria cachucha";
-		correctAnswer3 = "no tempo da maria cachucha";
-
 		destination = "GTEFRD.html";
 	} else if (level == "GTEFRD") {
 		correctAnswer = "obras de santa engrácia";
-		correctAnswer2 = "como as obras de santa engrácia";
-		correctAnswer3 = "como as obras de santa engrácia";
-
 		destination = "waiting.html";
 	}
 
